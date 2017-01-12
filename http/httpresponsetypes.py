d2f1f65fe1da1b9eb9c05dbc8c733ad54279ba7a
George Antonious <george@antonious.ca>
2017-01-11 19:13:47 -0700

    Add HttpResponse types

---
+++ http/httpresponsetypes.py
@@ -0,0 +1,13 @@
+from http.httpresponse import HttpResponse
+
+class OK(HttpResponse):
+    def __init__(self):
+        HttpResponse.__init__(self, 200, "OK")
+
+class NotFound(HttpResponse):
+    def __init__(self):
+        HttpResponse.__init__(self, 404, "Not Found")
+
+class MethodNotAllowed(HttpResponse):
+    def __init__(self):
+        HttpResponse.__init__(self, 405, "Method Not Allowed")
