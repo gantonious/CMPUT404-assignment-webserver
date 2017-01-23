3439076afe43240785ef97598a95a2d864d28a69
George Antonious <george@antonious.ca>
2017-01-23 11:04:15 -0700

    Add small delay to runner.sh for slower machines to ensure the server is running before launching tests

--- runner.sh
+++ runner.sh
@@ -2,6 +2,8 @@
 # Run the webserver, run the tests and kill the webserver!
 python server.py &
 ID=$!
+# sleep to give the server enough time to start up
+sleep 0.2
 python freetests.py
 python not-free-tests.py
 kill $ID
